5d2b4586713f7fdc35868e7ee00949e5d356155d
yjs2952 <yjs2952@naver.com>
2018-12-27 21:55:37 +0900

    [추가] 스프링부트 리액트 연동

--- src/main/java/com/kakaoix/todoapp/controller/MainController.java
+++ src/main/java/com/kakaoix/todoapp/controller/MainController.java
@@ -1,7 +1,9 @@
 package com.kakaoix.todoapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class MainController {
@@ -9,5 +11,11 @@ public class MainController {
     @GetMapping("/")
     public String todoList(){
         return "todoList";
+    }
+
+    @GetMapping("/{name}.html")
+    public String page(@PathVariable String name, Model model) {
+        model.addAttribute("pageName", name);
+        return "page";
     }
 }
