0cb79ef746d82b298042b61775c7551e009d2910
yjs2952 <yjs2952@naver.com>
2018-12-30 00:51:01 +0900

    [수정] todoList.html 수정

--- src/main/java/com/kakaoix/todoapp/TodoappApplication.java
+++ src/main/java/com/kakaoix/todoapp/TodoappApplication.java
@@ -8,30 +8,41 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
 
 @SpringBootApplication
-public class TodoappApplication  {
+public class TodoappApplication implements CommandLineRunner {
 
-	@Autowired
-	private TodoItemRepository todoItemRepository;
+    @Autowired
+    private TodoItemRepository todoItemRepository;
 
-	/*@Override
-	public void run(String... args) throws Exception {
-		//List<TodoItem> list = todoItemRepository.findAll();
-		*//*todoItemRepository.save(TodoItem.builder()
-				.content("테스트")
-				.isChecked(0)
-				.referenceItems(Collections.singletonList(todoItemRepository.getOne(2L)))
-				.status(Status.REF)
-				.build());*//*
+    @Override
+    public void run(String... args) throws Exception {
+        List<TodoItem> todoItems = new ArrayList<>();
+        todoItems.add(todoItemRepository.getOne(2L));
+        todoItems.add(todoItemRepository.getOne(3L));
 
-		//System.out.println(list.size());
-	}*/
+        IntStream.rangeClosed(1, 100).forEach(index -> {
+                    todoItemRepository.save(TodoItem.builder()
+                            .content("테스트" + index)
+                            .isChecked(1)
+                            //.referenceItems(todoItems)
+                            .status(Status.REF)
+                            .regDate(LocalDateTime.now())
+                            .modDate(LocalDateTime.now())
+                            .build());
+                }
+        );
+        //System.out.println(list.size());
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoappApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TodoappApplication.class, args);
+    }
 
 }
 
