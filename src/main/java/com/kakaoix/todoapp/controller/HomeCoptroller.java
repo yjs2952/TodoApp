4f70fc88dd9169f94830957a34dd395ac9280a67
yjs2952 <yjs2952@naver.com>
2018-12-29 16:33:12 +0900

    [수정] HomeController todo 목록 보기 구현

--- src/main/java/com/kakaoix/todoapp/controller/HomeCoptroller.java
+++ src/main/java/com/kakaoix/todoapp/controller/HomeCoptroller.java
@@ -1,13 +1,25 @@
 package com.kakaoix.todoapp.controller;
 
+import com.kakaoix.todoapp.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeCoptroller {
 
-    @GetMapping("/")
-    public String home(){
+    private TodoService todoService;
+
+    public HomeCoptroller(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping({"", "/"})
+    public String home(@PageableDefault Pageable pageable, Model model) {
+        model.addAttribute("todoList", todoService.getTodoList(pageable));
         return "todoList";
     }
 }
