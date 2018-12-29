c73fbc404db1704372805b54d3aeda48c93eb8ea
yjs2952 <yjs2952@naver.com>
2018-12-29 16:35:17 +0900

    [수정] HomeController -> TodoController 로 이름 변경

--- src/main/java/com/kakaoix/todoapp/controller/TodoController.java
+++ src/main/java/com/kakaoix/todoapp/controller/TodoController.java
@@ -1,7 +1,6 @@
 package com.kakaoix.todoapp.controller;
 
 import com.kakaoix.todoapp.service.TodoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
@@ -9,11 +8,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeCoptroller {
+public class TodoController {
 
     private TodoService todoService;
 
-    public HomeCoptroller(TodoService todoService) {
+    public TodoController(TodoService todoService) {
         this.todoService = todoService;
     }
 
