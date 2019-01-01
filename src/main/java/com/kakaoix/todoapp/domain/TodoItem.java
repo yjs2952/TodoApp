288f31f5d33c898ca1914d222be3a2bc874e29f9
yjs2952 <yjs2952@naver.com>
2019-01-01 13:31:28 +0900

    valid 테스트

--- src/main/java/com/kakaoix/todoapp/domain/TodoItem.java
+++ src/main/java/com/kakaoix/todoapp/domain/TodoItem.java
@@ -4,9 +4,9 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.List;
 
 @Entity @Table(name = "todo_item")
 @Getter @Setter
@@ -20,6 +20,9 @@ public class TodoItem {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // TODO: 2019-01-01 : validate 기능 작동은 하나 에러 메시지 반환이 안됨...... 
+    @Size(max = 255, message = "최대 255자를 넘을 수 없습니다.")
+    @NotEmpty(message = "Todo 명을 입력해 주세요.")
     @Column(nullable = false)
     private String content;
 
